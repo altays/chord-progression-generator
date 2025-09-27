48610bd81d54f817d2cb87f3b8a70c40d82a6585
Alex <altays633@gmail.com>
2025-09-27 09:31:58 -0400

    making chord creation functions modular

--- scripts/specific.js
+++ scripts/specific.js
@@ -1,55 +1,51 @@
 import  { Roots, TravelIntervals, ResIntervals, chordAlias, getRandomInt, getRandomRoot,getRandomInterval, getRandomChord, calculateInterval, wrapOctave, getNextRoot, getChords, getRandomChordType } from "./helper.js"
 
-// testing
+// starting chord
 
-let startRoot=getRandomRoot(Roots);
-let startType=getRandomChordType();
-let startChord=`${startRoot.note}${startType}`
-
-// resolving chord (built off of chord before)
-// random interval, random type based on type of target chord
-
-let resInterval=getRandomInterval(ResIntervals);
-let resRoot=Roots[getNextRoot(startRoot, resInterval.distance)]
-let resChordTypesArray=resInterval.list.chords.find((element) => element.targetChord == startType).tensionChord
-let resChordType = resChordTypesArray[getRandomInt(resChordTypesArray.length)]
-let resChord=`${resRoot.note}${resChordType}`
-
-// travel chord into resolving chord (if more than 2 chords)
-
-let travelInterval;
-let travelRoot;
-let travelChordTypeArray;
-let travelChordType
-while (travelChordTypeArray == undefined){
-    travelInterval=getRandomInterval(TravelIntervals);
-    travelRoot=Roots[getNextRoot(resRoot, travelInterval.distance)].note
-    travelChordTypeArray=travelInterval.list.chords.find((element) => element.targetChord == chordAlias(resChordType))
+function createStartChord() {
+    let startRoot=getRandomRoot(Roots);
+    let startType=getRandomChordType();
+    return {root: startRoot, type: startType}
 }
-travelChordType=travelChordTypeArray.tensionChord[getRandomInt(travelChordTypeArray.tensionChord.length)]
 
-let travelChord = `${travelRoot}${travelChordType}`
-console.log( startChord, travelChord, resChord)
+let chord1 = createStartChord()
+
+function createResChord(startChord, Roots, Intervals) {
+    let resInterval=getRandomInterval(Intervals);
+    let resRoot=Roots[getNextRoot(startChord.root, resInterval.distance)]
+    let resChordTypesArray=resInterval.list.chords.find((element) => element.targetChord == startChord.type).tensionChord
+    let resChordType = resChordTypesArray[getRandomInt(resChordTypesArray.length)]
+    return {root: resRoot, type: resChordType}
+}
+
+let lastChord = createResChord(chord1, Roots, ResIntervals)
+
+function createTravelChord(subChord, Roots, Intervals) {
+    let travelInterval;
+    let travelRoot;
+    let travelChordTypeArray;
+    let travelChordType
+    while (travelChordTypeArray == undefined){
+        travelInterval=getRandomInterval(Intervals);
+        travelRoot=Roots[getNextRoot(subChord.root, travelInterval.distance)]
+        travelChordTypeArray=travelInterval.list.chords.find((element) => element.targetChord == chordAlias(subChord.type))
+    }
+    travelChordType=travelChordTypeArray.tensionChord[getRandomInt(travelChordTypeArray.tensionChord.length)]
+    return {root: travelRoot, type: travelChordType}
+}
+
+let travelChord = createTravelChord(lastChord, Roots, TravelIntervals)
+
+console.log(chord1, travelChord, lastChord)
 
 // to dos
-    // make chord calculation modular
-        // start chord is whatever
-        // res chord is fine
-        // travel chord -> could be modular
     // configs
         // number of chords
     // chord order
         // currently is Start, Travel, Res by default
-        // could make order custom - like multiple resolution chords?
+        // could make order custom - like inserting a res chord before a travel chord?
 
 // later on
-// add other chord types onto list - sus chords, 6 chords
-
-// testing
-
-// const array = [{id:5,thing:"banana"}, {id:12, thing:"strawberry"}, {id:8}, {id:130}, {id:44}];
-
-// const found = array.find((element) => element.id > 10);
-
-// console.log(found.thing);
-// Expected output: 12
+    // add other chord types onto list - sus chords, 6 chords
+    // work out aug and dim chord motions for res chords
+    // look into why travel chord calculation is hanging
