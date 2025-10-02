69beb3e1e04270074af3e820e92fb1f2f8d29fae
Alex <altays633@gmail.com>
2025-10-02 18:08:24 -0400

    adding utility function to sh file

--- main.js
+++ main.js
@@ -4,9 +4,17 @@ import { progressionCreator } from './scripts/chord.mjs'
 // later on
     // add other chord types onto list - sus chords, 6 chords
     // look into why travel chord calculation is hanging
+    // have some way to export data into a file
 
 const chordPattern = process.argv[2]
 
+// selector script
+
+// logic to select chromatic progression
 let chordList = progressionCreator(chordPattern)
 console.log('chord list')
 console.log(chordList)
+
+//logic to select chord tropes
+    // condition of chord tropes to include
+
